df32a5db43c121fb55e004da065d454ef4e0c57e
h-genie <ses2000hj@hanmail.net>
2024-08-25 19:53:12 +0900

    26. 이미지 카드

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -5,12 +5,30 @@ export default function ReviewsPage() {
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul>
-        <li>
-          <Link href={"/reviews/hollow-knight"}>hollow-knight</Link>
+      <ul className="flex flex-col gap-3">
+        <li className="bg-white border shadow rounded w-80 hover:shadow-xl">
+          <Link href={"/reviews/hollow-knight"}>
+            <img
+              src="/images/hollow-knight.jpg"
+              alt=""
+              width={320}
+              height={180}
+              className="rounded-t"
+            />
+            <h2 className="py-1 text-center">Hollow-knight</h2>
+          </Link>
         </li>
-        <li>
-          <Link href={"/reviews/stardew-valley"}>Stardew Valley</Link>
+        <li className="bg-white border shadow rounded w-80 hover:shadow-xl">
+          <Link href={"/reviews/stardew-valley"}>
+            <img
+              src="/images/stardew-valley.jpg"
+              alt=""
+              width={320}
+              height={180}
+              className="rounded-t"
+            />
+            <h2 className="py-1 text-center">Stardew Valley</h2>
+          </Link>
         </li>
       </ul>
     </>
