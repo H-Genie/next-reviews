6725d73b85796d29883f7fbda9af8553380224fa
h-genie <ses2000hj@hanmail.net>
2024-09-11 20:47:18 +0900

    38. 배열 렌더링

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -8,35 +8,26 @@ export default async function ReviewsPage() {
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white border shadow rounded w-80 hover:shadow-xl">
-          <Link href={"/reviews/hollow-knight"}>
-            <img
-              src="/images/hollow-knight.jpg"
-              alt=""
-              width={320}
-              height={180}
-              className="rounded-t"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center">
-              Hollow-knight
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white border shadow rounded w-80 hover:shadow-xl">
-          <Link href={"/reviews/stardew-valley"}>
-            <img
-              src="/images/stardew-valley.jpg"
-              alt=""
-              width={320}
-              height={180}
-              className="rounded-t"
-            />
-            <h2 className="font-semibold font-orbitron py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map(review => (
+          <li
+            className="bg-white border shadow rounded w-80 hover:shadow-xl"
+            key={review.slug}
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width={320}
+                height={180}
+                className="rounded-t"
+              />
+              <h2 className="font-semibold font-orbitron py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   )
