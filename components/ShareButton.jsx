475c84a99218c08b7a354b91766ad7c16a521780
h-genie <ses2000hj@hanmail.net>
2024-09-23 20:48:00 +0900

    49. Hero Icons

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -5,7 +5,7 @@ import ShareLinkButton from "./ShareLinkButton"
 export default function ShareButton() {
   return (
     <div>
-      <ShareLinkButton /> | [Twitter] | [Facebook]
+      <ShareLinkButton />
     </div>
   )
 }
--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { LinkIcon } from "@heroicons/react/20/solid"
 
 export default function ShareLinkButton() {
   const [clicked, setClicked] = useState(false)
@@ -13,9 +14,10 @@ export default function ShareLinkButton() {
 
   return (
     <button
-      className="border px-2 py-1 rounded tetxt-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+      className="border flex gap-1 items-center px-2 py-1 rounded tetxt-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
       onClick={handleClick}
     >
+      <LinkIcon className="h-4 w-4" />
       {clicked ? "Link copied" : "Share Link"}
     </button>
   )
