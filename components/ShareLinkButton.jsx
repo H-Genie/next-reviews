a7c0b0da00c6ca0dbf5a4469bc900b4979a0bef3
h-genie <ses2000hj@hanmail.net>
2024-09-23 20:17:14 +0900

    47. Component state

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,8 +1,16 @@
 "use client"
 
+import { useState } from "react"
+
 export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false)
+
   const handleClick = () => {
-    console.log("click")
+    navigator.clipboard.writeText(window.location.href)
+    setClicked(true)
+    setTimeout(() => {
+      setClicked(false)
+    }, 1500)
   }
 
   return (
@@ -10,7 +18,7 @@ export default function ShareLinkButton() {
       className="border px-2 py-1 rounded tetxt-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
       onClick={handleClick}
     >
-      Share Link
+      {clicked ? "Link copied" : "Share Link"}
     </button>
   )
 }
