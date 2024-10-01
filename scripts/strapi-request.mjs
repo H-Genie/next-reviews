e8df38e36fdd5c58cce88290e838d850c18af0cc
h-genie <ses2000hj@hanmail.net>
2024-10-01 21:03:52 +0900

    60. Strapi API 매개변수

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,18 @@
 import { writeFileSync } from "node:fs"
+import qs from "qs"
 
-const url = `http://localhost:4000/api/reviews?populate=*`
+const url =
+  `http://localhost:4000/api/reviews` +
+  "?" +
+  qs.stringify(
+    {
+      fields: ["slug", "title", "subtitle", "publishedAt"],
+      populate: { image: { fields: ["url"] } },
+      sort: ["publishedAt:desc"],
+      pagination: { pageSize: 6 }
+    },
+    { encodeValuesOnly: true }
+  )
 const response = await fetch(url)
 const body = await response.json()
 const formatted = JSON.stringify(body, null, 2)
