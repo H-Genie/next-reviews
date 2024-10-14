da8ea615b71c077a2c43b6bda5d4a56826685cee
h-genie <ses2000hj@hanmail.net>
2024-10-14 21:15:13 +0900

    72. 홈 이미지

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,39 @@
 import Link from "next/link"
 import Heading from "@/components/Heading"
-import { getFeaturedReview } from "@/lib/review"
+import { getReviews } from "@/lib/review"
+import Image from "next/image"
 
 export default async function HomePage() {
-  const review = await getFeaturedReview()
+  const reviews = await getReviews(3)
   return (
     <>
       <Heading>Indie Game</Heading>
       <p className="pb-3">Only the best inside games, reviewed for you.</p>
-      <div className="bg-white border shadow w-80 rounded hover:shadow-xl sm:w-full">
-        <Link
-          href={`/reviews/${review.slug}`}
-          className="flex flex-col sm:flex-row"
-        >
-          <img
-            src={review.image}
-            alt=""
-            width={320}
-            height={180}
-            className="rounded-t sm:rounded-l sm:rounded-r-none"
-          />
-          <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
-            {review.title}
-          </h2>
-        </Link>
-      </div>
+      <ul className="flex flex-col gap-3">
+        {reviews.map((review, index) => (
+          <li
+            key={review.slug}
+            className="bg-white border shadow w-80 rounded hover:shadow-xl sm:w-full"
+          >
+            <Link
+              href={`/reviews/${review.slug}`}
+              className="flex flex-col sm:flex-row"
+            >
+              <Image
+                src={review.image}
+                alt=""
+                priority={index === 0}
+                width={320}
+                height={180}
+                className="rounded-t sm:rounded-l sm:rounded-r-none"
+              />
+              <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
