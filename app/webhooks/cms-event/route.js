5bd450ecf3aa1f84b2be1fb0d1b07bf21eb57412
h-genie <ses2000hj@hanmail.net>
2024-10-30 21:40:51 +0900

    81. 온디맨드 재검증

--- app/webhooks/cms-event/route.js
+++ app/webhooks/cms-event/route.js
@@ -1,8 +1,11 @@
-import { NextResponse } from "next/server"
+import { CACHE_TAG_REVIEWS } from "@/lib/review"
+import { revalidateTag } from "next/cache"
 
 export const POST = async req => {
   const payload = await req.json()
-  console.log(payload)
+  if (payload.mode === "review") {
+    revalidateTag(CACHE_TAG_REVIEWS)
+  }
 
   return new Response(null, { status: 204 })
 }
