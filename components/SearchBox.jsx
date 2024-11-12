e06c207970f2cc32fc927bf00a70a7c634dde324
h-genie <ses2000hj@hanmail.net>
2024-11-12 21:29:30 +0900

    90. Client-Only 렌더링

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,7 +1,11 @@
 "use client"
+import { useIsClient } from "@/lib/hooks"
 import { Combobox, ComboboxInput } from "@headlessui/react"
 
 export default function SearchBox() {
+  const isClient = useIsClient()
+
+  if (!isClient) return null
   return (
     <Combobox>
       <ComboboxInput placeholder="Search..." />
