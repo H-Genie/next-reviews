03f78e0d5ee0c5a2914ef4f876b48d95d4993cfb
h-genie <ses2000hj@hanmail.net>
2024-11-12 21:45:04 +0900

    91. Combobox 스타일

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,14 +1,47 @@
 "use client"
 import { useIsClient } from "@/lib/hooks"
-import { Combobox, ComboboxInput } from "@headlessui/react"
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions
+} from "@headlessui/react"
+
+const reviews = [
+  { slug: "hades-2018", title: "Hades" },
+  { slug: "fall-guys", title: "Fall Guys: Ultimate Knockout" },
+  { slug: "disco-elysium", title: "Disco Elysium" },
+  { slug: "dead-cells", title: "Dead Cells" },
+  { slug: "a-way-out-2018", title: "A Way Out" }
+]
 
 export default function SearchBox() {
   const isClient = useIsClient()
 
   if (!isClient) return null
   return (
-    <Combobox>
-      <ComboboxInput placeholder="Search..." />
-    </Combobox>
+    <div className="relative w-48">
+      <Combobox>
+        <ComboboxInput
+          placeholder="Search..."
+          className="border px-2 py-1 rounded w-full"
+        />
+        <ComboboxOptions className="absolute bg-white py-1 w-full">
+          {reviews.map(review => (
+            <ComboboxOption key={review.slug}>
+              {({ active }) => (
+                <span
+                  className={`block px-2 truncate w-full ${
+                    active ? "bg-orange-100" : ""
+                  }`}
+                >
+                  {review.title}
+                </span>
+              )}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
+      </Combobox>
+    </div>
   )
 }
