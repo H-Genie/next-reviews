ebc86e4d36cb0b5edaeb46c086ed1c5aef45436f
h-genie <ses2000hj@hanmail.net>
2024-11-13 21:36:21 +0900

    92. Combobox 이벤트

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -6,6 +6,8 @@ import {
   ComboboxOption,
   ComboboxOptions
 } from "@headlessui/react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 const reviews = [
   { slug: "hades-2018", title: "Hades" },
@@ -16,19 +18,27 @@ const reviews = [
 ]
 
 export default function SearchBox() {
+  const router = useRouter()
   const isClient = useIsClient()
+  const [query, setQuery] = useState("")
+
+  const handleChange = review => router.push(`/reviews/${review.slug}`)
+
+  const filtered = reviews.filter(review => review.title.includes(query))
 
   if (!isClient) return null
   return (
     <div className="relative w-48">
-      <Combobox>
+      <Combobox onChange={handleChange}>
         <ComboboxInput
+          value={query}
+          onChange={e => setQuery(e.target.value)}
           placeholder="Search..."
           className="border px-2 py-1 rounded w-full"
         />
         <ComboboxOptions className="absolute bg-white py-1 w-full">
-          {reviews.map(review => (
-            <ComboboxOption key={review.slug}>
+          {filtered.map(review => (
+            <ComboboxOption key={review.slug} value={review}>
               {({ active }) => (
                 <span
                   className={`block px-2 truncate w-full ${
